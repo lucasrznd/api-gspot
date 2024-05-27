49aef16343c2010c37a75cdf61654c68a36ed396
Lucas Rezende <lucasffrezende@gmail.com>
2024-05-27 16:24:49 -0300

    ✨ Criada estrutura do projeto

---
+++ src/main/java/com/lucasrznd/apigspot/ApiGspotApplication.java
@@ -0,0 +1,13 @@
+package com.lucasrznd.apigspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGspotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGspotApplication.class, args);
+    }
+
+}
