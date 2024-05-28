910f2016d1dd89a856480bd13cd9a966ed2d4139
Lucas Rezende <lucasffrezende@gmail.com>
2024-05-28 10:57:37 -0300

    ✨ Adicionado dto e mapper de locutor

---
+++ src/main/java/com/lucasrznd/apigspot/dtos/AnnouncerDTO.java
@@ -0,0 +1,11 @@
+package com.lucasrznd.apigspot.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+
+public record AnnouncerDTO(Long id,
+                           @NotEmpty(message = "Name can't be empty.")
+                           @NotBlank(message = "Name can't be blank.") String name,
+                           String phoneNumber,
+                           String urlImage) {
+}
---
+++ src/main/java/com/lucasrznd/apigspot/dtos/mappers/AnnouncerMapper.java
@@ -0,0 +1,32 @@
+package com.lucasrznd.apigspot.dtos.mappers;
+
+import com.lucasrznd.apigspot.dtos.AnnouncerDTO;
+import com.lucasrznd.apigspot.models.AnnouncerModel;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnnouncerMapper {
+
+    public AnnouncerDTO toDTO(AnnouncerModel announcer) {
+        if (announcer == null) {
+            return null;
+        }
+
+        return new AnnouncerDTO(announcer.getId(), announcer.getName(), announcer.getPhoneNumber(), announcer.getUrlImage());
+    }
+
+    public AnnouncerModel toModel(AnnouncerDTO announcerDTO) {
+        if (announcerDTO == null) {
+            return null;
+        }
+
+        AnnouncerModel announcerModel = new AnnouncerModel();
+        announcerModel.setId(announcerDTO.id());
+        announcerModel.setName(announcerDTO.name());
+        announcerModel.setPhoneNumber(announcerDTO.phoneNumber());
+        announcerModel.setUrlImage(announcerDTO.urlImage());
+
+        return announcerModel;
+    }
+
+}
