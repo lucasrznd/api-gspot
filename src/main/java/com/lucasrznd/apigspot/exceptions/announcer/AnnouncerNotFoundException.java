5dd11070b16fd98a7503fd88d884c5b5225c77c3
Lucas Rezende <lucasffrezende@gmail.com>
2024-05-29 08:55:38 -0300

    ✨ Adicionado exceptions de locutor

---
+++ src/main/java/com/lucasrznd/apigspot/exceptions/announcer/AnnouncerNotFoundException.java
@@ -0,0 +1,9 @@
+package com.lucasrznd.apigspot.exceptions.announcer;
+
+public class AnnouncerNotFoundException extends RuntimeException {
+
+    public AnnouncerNotFoundException(String msg) {
+        super(msg);
+    }
+
+}
---
+++ src/main/java/com/lucasrznd/apigspot/exceptions/announcer/DuplicateAnnouncerException.java
@@ -0,0 +1,9 @@
+package com.lucasrznd.apigspot.exceptions.announcer;
+
+public class DuplicateAnnouncerException extends RuntimeException {
+
+    public DuplicateAnnouncerException(String msg) {
+        super(msg);
+    }
+
+}
