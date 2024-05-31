d0d978b166b73c6e2ac8b05115ff99e50b1c2ab9
Lucas Rezende <lucasffrezende@gmail.com>
2024-05-31 19:28:20 -0300

    ✨ Adicionado metodo findByName

--- src/main/java/com/lucasrznd/apigspot/repositories/CompanyRepository.java
+++ src/main/java/com/lucasrznd/apigspot/repositories/CompanyRepository.java
@@ -4,4 +4,7 @@ import com.lucasrznd.apigspot.models.CompanyModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CompanyRepository extends JpaRepository<CompanyModel, Long> {
+
+    CompanyModel findByName(String name);
+
 }
