c6d4f3c2b742c7e34f782cf8554519728fc42b17
Lucas Rezende <lucasffrezende@gmail.com>
2024-05-31 19:29:19 -0300

    ✨ Adicionado exceptions de empresa

---
+++ src/main/java/com/lucasrznd/apigspot/exceptions/company/CompanyNotFoundException.java
@@ -0,0 +1,9 @@
+package com.lucasrznd.apigspot.exceptions.company;
+
+public class CompanyNotFoundException extends RuntimeException{
+
+    public CompanyNotFoundException(String msg) {
+        super(msg);
+    }
+
+}
---
+++ src/main/java/com/lucasrznd/apigspot/exceptions/company/DuplicateCompanyException.java
@@ -0,0 +1,9 @@
+package com.lucasrznd.apigspot.exceptions.company;
+
+public class DuplicateCompanyException extends RuntimeException{
+
+    public DuplicateCompanyException(String msg) {
+        super(msg);
+    }
+
+}
