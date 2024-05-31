9ee9a9e6dbec93b4a1567d2af12c11f477702be5
Lucas Rezende <lucasffrezende@gmail.com>
2024-05-31 19:29:47 -0300

    🎨 Tratamento de exceptions de empresa

--- src/main/java/com/lucasrznd/apigspot/handlers/GlobalExceptionHandler.java
+++ src/main/java/com/lucasrznd/apigspot/handlers/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.lucasrznd.apigspot.handlers;
 
 import com.lucasrznd.apigspot.exceptions.announcer.AnnouncerNotFoundException;
 import com.lucasrznd.apigspot.exceptions.announcer.DuplicateAnnouncerException;
+import com.lucasrznd.apigspot.exceptions.company.CompanyNotFoundException;
+import com.lucasrznd.apigspot.exceptions.company.DuplicateCompanyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -19,4 +21,15 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleDuplicateAnnouncerException(DuplicateAnnouncerException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
     }
+
+    @ExceptionHandler(CompanyNotFoundException.class)
+    public ResponseEntity<String> handleCompanyNotFoundException(CompanyNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(DuplicateCompanyException.class)
+    public ResponseEntity<String> handleDuplicateCompanyException(DuplicateCompanyException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+
 }
