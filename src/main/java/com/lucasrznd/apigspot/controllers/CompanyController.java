f5fd17c4c05b63928397705fabd382ea8f1873ad
Lucas Rezende <lucasffrezende@gmail.com>
2024-05-31 19:30:07 -0300

    ✨ Adicionado controller de empresa

---
+++ src/main/java/com/lucasrznd/apigspot/controllers/CompanyController.java
@@ -0,0 +1,51 @@
+package com.lucasrznd.apigspot.controllers;
+
+import com.lucasrznd.apigspot.dtos.CompanyDTO;
+import com.lucasrznd.apigspot.services.CompanyService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/company")
+@CrossOrigin(origins = "${frontend.client.url}")
+public class CompanyController {
+
+    private CompanyService companyService;
+
+    public CompanyController(CompanyService companyService) {
+        this.companyService = companyService;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<CompanyDTO> selectAll() {
+        return companyService.selectAll();
+    }
+
+    @GetMapping("/count")
+    @ResponseStatus(HttpStatus.OK)
+    public Long countCompanies() {
+        return companyService.countCompanies();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CompanyDTO insert(@RequestBody CompanyDTO companyDTO) {
+        return companyService.insert(companyDTO);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public CompanyDTO update(@PathVariable Long id, @RequestBody CompanyDTO companyDTO) {
+        return companyService.update(id, companyDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void delete(@PathVariable Long id) {
+        companyService.delete(id);
+    }
+
+}
