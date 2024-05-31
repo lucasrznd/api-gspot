f0ac6291e9c67563299e1a0309a45dbf9f3f0c27
Lucas Rezende <lucasffrezende@gmail.com>
2024-05-31 19:27:34 -0300

    ✨ Adicionado dto e mapper de empresa

---
+++ src/main/java/com/lucasrznd/apigspot/dtos/mappers/CompanyMapper.java
@@ -0,0 +1,32 @@
+package com.lucasrznd.apigspot.dtos.mappers;
+
+import com.lucasrznd.apigspot.dtos.CompanyDTO;
+import com.lucasrznd.apigspot.models.CompanyModel;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CompanyMapper {
+
+    public CompanyDTO toDTO(CompanyModel company) {
+        if (company == null) {
+            return null;
+        }
+
+        return new CompanyDTO(company.getId(), company.getName(), company.getPhoneNumber(), company.getUrlImage());
+    }
+
+    public CompanyModel toModel(CompanyDTO companyDTO) {
+        if (companyDTO == null) {
+            return null;
+        }
+
+        CompanyModel companyModel = new CompanyModel();
+        companyModel.setId(companyDTO.id());
+        companyModel.setName(companyDTO.name());
+        companyModel.setPhoneNumber(companyDTO.phoneNumber());
+        companyModel.setUrlImage(companyDTO.urlImage());
+
+        return companyModel;
+    }
+
+}
