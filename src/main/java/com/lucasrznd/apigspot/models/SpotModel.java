c95ac9c1373b8a098d0dce729fdade3349e248cd
Lucas Rezende <lucasffrezende@gmail.com>
2024-06-03 10:26:47 -0300

    🎨 Alterado validações de SpotModel

--- src/main/java/com/lucasrznd/apigspot/models/SpotModel.java
+++ src/main/java/com/lucasrznd/apigspot/models/SpotModel.java
@@ -2,6 +2,7 @@ package com.lucasrznd.apigspot.models;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -22,29 +23,26 @@ public class SpotModel implements Serializable {
     @NotBlank(message = "Name can't be blank.")
     private String title;
 
-    @NotBlank(message = "Company can't be blank.")
+    @NotNull(message = "Company can't be null.")
     @ManyToOne
     @JoinColumn(name = "company_id")
     private CompanyModel company;
 
-    @NotBlank(message = "Announcer can't be blank.")
+    @NotNull(message = "Announcer can't be null.")
     @ManyToOne
     @JoinColumn(name = "announcer_id")
     private AnnouncerModel announcer;
 
-    @NotBlank(message = "Date can't be blank.")
     @Column(name = "date")
     private LocalDate date;
 
-    @NotBlank(message = "Duration can't be blank.")
+    @NotNull(message = "Duration can't be blank.")
     @Column(name = "duration")
     private Double duration;
 
-    @NotBlank(message = "Active Contract can't be blank.")
     @Column(name = "active_contract")
     private boolean activeContract;
 
-    @NotBlank(message = "Price can't be blank.")
     @Column(name = "price")
     private BigDecimal price;
 
