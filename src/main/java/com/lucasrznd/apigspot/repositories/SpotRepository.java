53765042f4d089d869f3114522d2cb79ca116bd9
Lucas Rezende <lucasffrezende@gmail.com>
2024-06-07 18:16:29 -0300

    ✨ Adicionado getAmountRaised

--- src/main/java/com/lucasrznd/apigspot/repositories/SpotRepository.java
+++ src/main/java/com/lucasrznd/apigspot/repositories/SpotRepository.java
@@ -2,6 +2,13 @@ package com.lucasrznd.apigspot.repositories;
 
 import com.lucasrznd.apigspot.models.SpotModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.math.BigDecimal;
 
 public interface SpotRepository extends JpaRepository<SpotModel, Long> {
+
+    @Query("SELECT SUM(s.price) FROM SpotModel s")
+    BigDecimal getAmountRaised();
+
 }
