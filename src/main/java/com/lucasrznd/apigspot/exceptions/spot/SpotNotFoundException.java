16948d8758e03ecdb175020e71d3fe46cc72dafd
Lucas Rezende <lucasffrezende@gmail.com>
2024-06-07 18:19:38 -0300

    ✨ Adicionado exceptions de spot

---
+++ src/main/java/com/lucasrznd/apigspot/exceptions/spot/SpotNotFoundException.java
@@ -0,0 +1,9 @@
+package com.lucasrznd.apigspot.exceptions.spot;
+
+public class SpotNotFoundException extends RuntimeException {
+
+    public SpotNotFoundException(String msg) {
+        super(msg);
+    }
+
+}
