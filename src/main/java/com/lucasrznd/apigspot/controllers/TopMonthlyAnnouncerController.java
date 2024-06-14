9abc7fedb3aa87003ac18630c8681d3762128fc4
Lucas Rezende <lucasffrezende@gmail.com>
2024-06-14 15:37:10 -0300

    ✨ Adicionado controller de TopAnnouncer

---
+++ src/main/java/com/lucasrznd/apigspot/controllers/TopMonthlyAnnouncerController.java
@@ -0,0 +1,29 @@
+package com.lucasrznd.apigspot.controllers;
+
+import com.lucasrznd.apigspot.models.TopMonthlyAnnouncer;
+import com.lucasrznd.apigspot.services.TopMonthlyAnnouncerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("monthly/announcer")
+public class TopMonthlyAnnouncerController {
+
+    private final TopMonthlyAnnouncerService topMonthlyAnnouncerService;
+
+    public TopMonthlyAnnouncerController(TopMonthlyAnnouncerService topMonthlyAnnouncerService) {
+        this.topMonthlyAnnouncerService = topMonthlyAnnouncerService;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<TopMonthlyAnnouncer> findTopMonthlyAnnouncers() {
+        return topMonthlyAnnouncerService.findTopMonthlyAnnouncers();
+    }
+
+}
