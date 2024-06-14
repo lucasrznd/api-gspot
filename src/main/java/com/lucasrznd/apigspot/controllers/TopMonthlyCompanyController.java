228c29af1ad62685e898803e879878187d84bb2e
Lucas Rezende <lucasffrezende@gmail.com>
2024-06-14 17:13:56 -0300

    ✨ Adicionado controller de TopCompany

---
+++ src/main/java/com/lucasrznd/apigspot/controllers/TopMonthlyCompanyController.java
@@ -0,0 +1,30 @@
+package com.lucasrznd.apigspot.controllers;
+
+import com.lucasrznd.apigspot.models.TopMonthlyCompany;
+import com.lucasrznd.apigspot.services.TopMonthlyAnnouncerService;
+import com.lucasrznd.apigspot.services.TopMonthlyCompanyService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/monthly/company")
+public class TopMonthlyCompanyController {
+
+    private final TopMonthlyCompanyService topMonthlyCompanyService;
+
+    public TopMonthlyCompanyController(TopMonthlyCompanyService topMonthlyCompanyService) {
+        this.topMonthlyCompanyService = topMonthlyCompanyService;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<TopMonthlyCompany> findTopMonthlyCompanies() {
+        return topMonthlyCompanyService.findTopMonthlyCompanies();
+    }
+
+}
