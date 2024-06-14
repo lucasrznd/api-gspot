5248c4f7f60bc9136b1925becfe621d60623690a
Lucas Rezende <lucasffrezende@gmail.com>
2024-06-14 15:36:35 -0300

    ✨ Adicionado repository de TopAnnouncer

---
+++ src/main/java/com/lucasrznd/apigspot/repositories/TopMonthlyAnnouncerRepository.java
@@ -0,0 +1,14 @@
+package com.lucasrznd.apigspot.repositories;
+
+import com.lucasrznd.apigspot.models.TopMonthlyAnnouncer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface TopMonthlyAnnouncerRepository extends JpaRepository<TopMonthlyAnnouncer, String> {
+
+    @Query(nativeQuery = true, value = "SELECT * FROM top_monthly_announcers")
+    List<TopMonthlyAnnouncer> findTopMonthlyAnnouncers();
+
+}
