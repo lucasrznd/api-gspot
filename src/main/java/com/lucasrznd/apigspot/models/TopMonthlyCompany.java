c7283301dddffc7d36aa8841f92c292e557b9d99
Lucas Rezende <lucasffrezende@gmail.com>
2024-06-14 17:12:49 -0300

    ✨ Adicionado TopMonthlyCompany

---
+++ src/main/java/com/lucasrznd/apigspot/models/TopMonthlyCompany.java
@@ -0,0 +1,25 @@
+package com.lucasrznd.apigspot.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "top_monthly_company")
+public class TopMonthlyCompany implements Serializable {
+
+    @Id
+    @Column(name = "company_name")
+    private String companyName;
+
+    @Column(name = "spot_count")
+    private int spotCount;
+
+}
