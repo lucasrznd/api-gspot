be2c3f4126a1bbdbbaa87feff46d1c914461f283
Lucas Rezende <lucasffrezende@gmail.com>
2024-07-03 21:48:28 -0300

    ✨ Adicionar metodo equals

--- src/main/java/com/lucasrznd/apigspot/models/AnnouncerModel.java
+++ src/main/java/com/lucasrznd/apigspot/models/AnnouncerModel.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.commons.lang3.builder.EqualsBuilder;
 
 @Getter
 @Setter
@@ -18,6 +19,11 @@ public class AnnouncerModel extends GenericModel {
 
     public AnnouncerModel(Long id, @NotEmpty(message = "Name can't be empty.") @NotBlank(message = "Name can't be blank.") String name, String phoneNumber, String urlImage) {
         super(id, name, phoneNumber, urlImage);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(obj, this);
     }
 
 }
