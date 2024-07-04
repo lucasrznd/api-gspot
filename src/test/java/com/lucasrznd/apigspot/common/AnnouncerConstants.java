3f01fdcabf78592ab376cdf01c8204f2c88883eb
Lucas Rezende <lucasffrezende@gmail.com>
2024-07-03 21:57:11 -0300

    🧪 Adicionar constante de announcer para testes

---
+++ src/test/java/com/lucasrznd/apigspot/common/AnnouncerConstants.java
@@ -0,0 +1,9 @@
+package com.lucasrznd.apigspot.common;
+
+import com.lucasrznd.apigspot.models.AnnouncerModel;
+
+public class AnnouncerConstants {
+
+    public static final AnnouncerModel ANNOUNCER = new AnnouncerModel(null, "Lucas Rezende", "43999999999", "");
+
+}
