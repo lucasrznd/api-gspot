c6d5925f76ed1159aa041a2cc5ad6c626a44d8e9
Lucas Rezende <lucasffrezende@gmail.com>
2024-07-04 21:57:15 -0300

    ✨ Adicionar novos handlers exceptions

--- src/main/java/com/lucasrznd/apigspot/handlers/GlobalExceptionHandler.java
+++ src/main/java/com/lucasrznd/apigspot/handlers/GlobalExceptionHandler.java
@@ -2,8 +2,12 @@ package com.lucasrznd.apigspot.handlers;
 
 import com.lucasrznd.apigspot.exceptions.announcer.AnnouncerNotFoundException;
 import com.lucasrznd.apigspot.exceptions.announcer.DuplicateAnnouncerException;
+import com.lucasrznd.apigspot.exceptions.common.NameAlreadyExistsException;
+import com.lucasrznd.apigspot.exceptions.common.PhoneNumberAlreadyExistsException;
 import com.lucasrznd.apigspot.exceptions.company.CompanyNotFoundException;
 import com.lucasrznd.apigspot.exceptions.company.DuplicateCompanyException;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,23 +16,43 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(ConstraintViolationException.class)
+    private ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException ex){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    private ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(NameAlreadyExistsException.class)
+    private ResponseEntity<String> handleNameAlreadyExistsException(NameAlreadyExistsException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(PhoneNumberAlreadyExistsException.class)
+    private ResponseEntity<String> handlePhoneNumberAlreadyExistsException(PhoneNumberAlreadyExistsException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+
     @ExceptionHandler(AnnouncerNotFoundException.class)
-    public ResponseEntity<String> handleAnnouncerNotFoundException(AnnouncerNotFoundException ex) {
+    private ResponseEntity<String> handleAnnouncerNotFoundException(AnnouncerNotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(DuplicateAnnouncerException.class)
-    public ResponseEntity<String> handleDuplicateAnnouncerException(DuplicateAnnouncerException ex) {
+    private ResponseEntity<String> handleDuplicateAnnouncerException(DuplicateAnnouncerException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(CompanyNotFoundException.class)
-    public ResponseEntity<String> handleCompanyNotFoundException(CompanyNotFoundException ex) {
+    private ResponseEntity<String> handleCompanyNotFoundException(CompanyNotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(DuplicateCompanyException.class)
-    public ResponseEntity<String> handleDuplicateCompanyException(DuplicateCompanyException ex) {
+    private ResponseEntity<String> handleDuplicateCompanyException(DuplicateCompanyException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
     }
 
