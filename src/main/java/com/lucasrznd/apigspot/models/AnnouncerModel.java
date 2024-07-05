b40223929408f547052b6b7875c1c5cf0b117d51
Lucas Rezende <lucasffrezende@gmail.com>
2024-07-04 21:45:38 -0300

    ♻️ Refatorar os construtores

--- src/main/java/com/lucasrznd/apigspot/models/AnnouncerModel.java
+++ src/main/java/com/lucasrznd/apigspot/models/AnnouncerModel.java
@@ -2,22 +2,20 @@ package com.lucasrznd.apigspot.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "tb_locutor")
 public class AnnouncerModel extends GenericModel {
 
-    public AnnouncerModel() {
-    }
-
-    public AnnouncerModel(Long id, @NotEmpty(message = "Name can't be empty.") @NotBlank(message = "Name can't be blank.") String name, String phoneNumber, String urlImage) {
+    public AnnouncerModel(Long id, @NotEmpty(message = "Name can't be blank.") String name, String phoneNumber, String urlImage) {
         super(id, name, phoneNumber, urlImage);
     }
 
