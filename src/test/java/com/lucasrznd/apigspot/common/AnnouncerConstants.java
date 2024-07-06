ee4eb53505c640e2f21ccaca67c63643abb9396d
Lucas Rezende <lucasffrezende@gmail.com>
2024-07-05 22:28:24 -0300

    🧪 Adicionar constante para testes

--- src/test/java/com/lucasrznd/apigspot/common/AnnouncerConstants.java
+++ src/test/java/com/lucasrznd/apigspot/common/AnnouncerConstants.java
@@ -1,9 +1,13 @@
 package com.lucasrznd.apigspot.common;
 
+import com.lucasrznd.apigspot.dtos.AnnouncerDTO;
 import com.lucasrznd.apigspot.models.AnnouncerModel;
 
 public class AnnouncerConstants {
 
     public static final AnnouncerModel ANNOUNCER = new AnnouncerModel(null, "Lucas Rezende", "43999999999", "");
+    public static final AnnouncerDTO ANNOUNCER_DTO = new AnnouncerDTO(null, "Lucas Rezende", "43999999999", "");
+
+    public static final AnnouncerModel JOHN_DOE = new AnnouncerModel(2L, "John Doe", "43988888888", "");
 
 }
