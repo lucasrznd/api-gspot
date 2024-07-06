b8ab642418e2409ba1b8429f78784a9f7790fb2f
Lucas Rezende <lucasffrezende@gmail.com>
2024-07-05 22:35:07 -0300

    🧪 Adicionar script sql para testes

---
+++ src/test/resources/import_announcers.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_locutor (id, name, phone_number, url_image) VALUES (1, 'Lucas Rezende', '43999999999', '');
+INSERT INTO tb_locutor (id, name, phone_number, url_image) VALUES (2, 'John Doe', '43988888888', '');
+INSERT INTO tb_locutor (id, name, phone_number, url_image) VALUES (3, 'Bob Green', '43977777777', '');
