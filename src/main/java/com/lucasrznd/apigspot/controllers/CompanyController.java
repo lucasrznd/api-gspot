60fefedc02730e7758dbfe8e40e6690be581558a
Lucas Rezende <lucasffrezende@gmail.com>
2024-07-12 08:31:13 -0300

    ♻️ Refatorar company controller

--- src/main/java/com/lucasrznd/apigspot/controllers/CompanyController.java
+++ src/main/java/com/lucasrznd/apigspot/controllers/CompanyController.java
@@ -2,8 +2,17 @@ package com.lucasrznd.apigspot.controllers;
 
 import com.lucasrznd.apigspot.dtos.CompanyDTO;
 import com.lucasrznd.apigspot.services.CompanyService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -31,18 +40,18 @@ public class CompanyController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public CompanyDTO insert(@RequestBody CompanyDTO companyDTO) {
+    public CompanyDTO insert(@RequestBody @Valid CompanyDTO companyDTO) {
         return companyService.insert(companyDTO);
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public CompanyDTO update(@PathVariable Long id, @RequestBody CompanyDTO companyDTO) {
+    public CompanyDTO update(@PathVariable Long id, @RequestBody @Valid CompanyDTO companyDTO) {
         return companyService.update(id, companyDTO);
     }
 
     @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) {
         companyService.delete(id);
     }
