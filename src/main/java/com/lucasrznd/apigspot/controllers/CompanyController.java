513f0ea8e2466a5adade560dbf97e705ca5f7100
Lucas Rezende <lucasffrezende@gmail.com>
2024-07-17 18:29:03 -0300

    refactor: change annotation to response entity in company controller

--- src/main/java/com/lucasrznd/apigspot/controllers/CompanyController.java
+++ src/main/java/com/lucasrznd/apigspot/controllers/CompanyController.java
@@ -3,57 +3,47 @@ package com.lucasrznd.apigspot.controllers;
 import com.lucasrznd.apigspot.dtos.request.CompanyDTO;
 import com.lucasrznd.apigspot.services.CompanyService;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequestMapping("/company")
 public class CompanyController {
 
-    private final CompanyService companyService;
+    private final CompanyService service;
 
-    public CompanyController(CompanyService companyService) {
-        this.companyService = companyService;
+    public CompanyController(CompanyService service) {
+        this.service = service;
     }
 
     @GetMapping
-    @ResponseStatus(HttpStatus.OK)
-    public List<CompanyDTO> findAll() {
-        return companyService.findAll();
+    public ResponseEntity<List<CompanyDTO>> findAll() {
+        return ResponseEntity.ok().body(service.findAll());
     }
 
     @GetMapping("/count")
-    @ResponseStatus(HttpStatus.OK)
-    public Long count() {
-        return companyService.count();
+    public ResponseEntity<Long> count() {
+        return ResponseEntity.ok().body(service.count());
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public CompanyDTO save(@RequestBody @Valid CompanyDTO companyDTO) {
-        return companyService.save(companyDTO);
+    public ResponseEntity<CompanyDTO> save(@RequestBody @Valid CompanyDTO companyDTO) {
+        return ResponseEntity.status(CREATED).body(service.save(companyDTO));
     }
 
     @PutMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public CompanyDTO update(@PathVariable Long id, @RequestBody @Valid CompanyDTO companyDTO) {
-        return companyService.update(id, companyDTO);
+    public ResponseEntity<CompanyDTO> update(@PathVariable Long id, @RequestBody @Valid CompanyDTO companyDTO) {
+        return ResponseEntity.ok().body(service.update(id, companyDTO));
     }
 
     @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Long id) {
-        companyService.delete(id);
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
