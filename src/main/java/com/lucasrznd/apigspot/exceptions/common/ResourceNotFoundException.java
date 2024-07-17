cb12d37b17c135458df44597125e6da5ceb379e3
Lucas Rezende <lucasffrezende@gmail.com>
2024-07-17 07:56:56 -0300

    feat: add new structure for exceptions

---
+++ src/main/java/com/lucasrznd/apigspot/exceptions/common/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.lucasrznd.apigspot.exceptions.common;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+}
