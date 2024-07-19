c0123893b8715418d175982220e48dbdd6ef8399
Lucas Rezende <lucasffrezende@gmail.com>
2024-07-19 09:51:07 -0300

    test: add more spots constants

--- src/test/java/com/lucasrznd/apigspot/common/SpotConstants.java
+++ src/test/java/com/lucasrznd/apigspot/common/SpotConstants.java
@@ -1,6 +1,7 @@
 package com.lucasrznd.apigspot.common;
 
 import com.lucasrznd.apigspot.dtos.request.SpotDTO;
+import com.lucasrznd.apigspot.dtos.response.SpotResponse;
 import com.lucasrznd.apigspot.models.SpotModel;
 
 import java.time.LocalDate;
@@ -13,12 +14,15 @@ import static com.lucasrznd.apigspot.common.CompanyConstants.*;
 public class SpotConstants {
 
     public static final SpotModel SPOT =
+            new SpotModel(null, "QUINTA DA CARNE", COMPANY, ANNOUNCER, LocalDate.of(2024, 7, 11), 0.45, true, null);
+    public static final SpotModel SPOT_WITH_NON_NULL_PRICE =
             new SpotModel(null, "QUINTA DA CARNE", COMPANY, ANNOUNCER, LocalDate.of(2024, 7, 11), 0.45, true, 25.00);
     public static final SpotModel SPOT_MAGALU =
             new SpotModel(2L, "OFERTAS DE VERAO", MAGAZINE_LUIZA, ANNOUNCER, LocalDate.of(2024, 7, 4), 1.10, true, 25.00);
+    public static final List<SpotModel> SPOT_LIST = Arrays.asList(SPOT, SPOT_MAGALU);
 
     public static final SpotDTO SPOT_DTO =
-            new SpotDTO(null, "QUINTA DA CARNE", COMPANY_DTO, ANNOUNCER_DTO, LocalDate.of(2024, 7, 11), 0.45, true, 25.00);
+            new SpotDTO(null, "QUINTA DA CARNE", COMPANY_DTO, ANNOUNCER_DTO, LocalDate.of(2024, 7, 11), 0.45, true, null);
     public static final SpotDTO SPOT_MAGALU_DTO =
             new SpotDTO(2L, "OFERTAS DE VERAO", MAGAZINE_LUIZA_DTO, ANNOUNCER_DTO, LocalDate.of(2024, 7, 4), 1.10, true, 25.00);
     public static final SpotDTO INVALID_SPOT_DTO =
@@ -26,6 +30,10 @@ public class SpotConstants {
     public static final SpotDTO NULL_SPOT_DTO =
             new SpotDTO(null, null, null, null, null, null, false, null);
 
-    public static final List<SpotDTO> SPOT_DTO_LIST = Arrays.asList(SPOT_DTO, SPOT_MAGALU_DTO);
+    public static final SpotResponse SPOT_RESPONSE =
+            new SpotResponse(null, "QUINTA DA CARNE", COMPANY_DTO.name(), COMPANY_DTO.urlImage(), ANNOUNCER_DTO.name(), ANNOUNCER_DTO.urlImage(), LocalDate.of(2024, 7, 11), 0.45, true, 25.00);
+    public static final SpotResponse SPOT_MAGALU_RESPONSE =
+            new SpotResponse(2L, "OFERTAS DE VERAO", MAGAZINE_LUIZA_DTO.name(), MAGAZINE_LUIZA_DTO.urlImage(), ANNOUNCER_DTO.name(), ANNOUNCER_DTO.urlImage(), LocalDate.of(2024, 7, 4), 1.10, true, 25.00);
+    public static final List<SpotResponse> SPOT_RESPONSE_LIST = Arrays.asList(SPOT_RESPONSE, SPOT_MAGALU_RESPONSE);
 
 }
--- src/test/java/com/lucasrznd/apigspot/repositories/SpotRepositoryTest.java
+++ src/test/java/com/lucasrznd/apigspot/repositories/SpotRepositoryTest.java
@@ -1,7 +1,5 @@
 package com.lucasrznd.apigspot.repositories;
 
-import com.lucasrznd.apigspot.models.AnnouncerModel;
-import com.lucasrznd.apigspot.models.CompanyModel;
 import com.lucasrznd.apigspot.models.SpotModel;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -11,15 +9,13 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.util.List;
 
 import static com.lucasrznd.apigspot.common.AnnouncerConstants.ANNOUNCER;
 import static com.lucasrznd.apigspot.common.CompanyConstants.COMPANY;
 import static com.lucasrznd.apigspot.common.SpotConstants.SPOT;
+import static com.lucasrznd.apigspot.common.SpotConstants.SPOT_WITH_NON_NULL_PRICE;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 @DataJpaTest
 @ActiveProfiles("test")
@@ -58,88 +54,16 @@ public class SpotRepositoryTest {
     }
 
     @Test
-    public void createSpot_WithInvalidData_ThrowsException() {
-        SpotModel emptySpot = new SpotModel();
-        SpotModel invalidSpot = new SpotModel(null, null, new CompanyModel(), new AnnouncerModel(), null, null, false, null);
-
-        assertThatThrownBy(() -> spotRepository.save(emptySpot)).isInstanceOf(RuntimeException.class);
-        assertThatThrownBy(() -> spotRepository.save(invalidSpot)).isInstanceOf(RuntimeException.class);
-    }
-
-    @Test
-    public void createSpot_WithInvalidTitle_ThrowsException() {
-        companyRepository.save(COMPANY);
-        announcerRepository.save(ANNOUNCER);
-
-        SpotModel spotWithInvalidTitle = new SpotModel(null, "", COMPANY,
-                ANNOUNCER, LocalDate.now(), 0.45, true, 20.00);
-
-        assertThatThrownBy(() -> spotRepository.save(spotWithInvalidTitle)).isInstanceOf(RuntimeException.class);
-    }
-
-    @Test
-    public void createSpot_WithInvalidCompany_ThrowsException() {
-        announcerRepository.save(ANNOUNCER);
-
-        SpotModel spotWithInvalidCompany = new SpotModel(null, "", COMPANY,
-                ANNOUNCER, LocalDate.now(), 0.45, true, 20.00);
-
-        assertThatThrownBy(() -> spotRepository.save(spotWithInvalidCompany)).isInstanceOf(RuntimeException.class);
-    }
-
-    @Test
-    public void createSpot_WithInvalidAnnouncer_ThrowsException() {
-        companyRepository.save(COMPANY);
-
-        SpotModel spotWithInvalidAnnouncer = new SpotModel(null, "", COMPANY,
-                ANNOUNCER, LocalDate.now(), 0.45, true, 20.00);
-
-        assertThatThrownBy(() -> spotRepository.save(spotWithInvalidAnnouncer)).isInstanceOf(RuntimeException.class);
-    }
-
-    @Test
-    public void createSpot_WithInvalidDate_ThrowsException() {
-        companyRepository.save(COMPANY);
-        announcerRepository.save(ANNOUNCER);
-
-        SpotModel spotWithInvalidDate = new SpotModel(null, "", COMPANY,
-                ANNOUNCER, null, 0.45, true, 20.00);
-
-        assertThatThrownBy(() -> spotRepository.save(spotWithInvalidDate)).isInstanceOf(RuntimeException.class);
-    }
-
-    @Test
-    public void createSpot_WithInvalidDuration_ThrowsException() {
-        companyRepository.save(COMPANY);
-        announcerRepository.save(ANNOUNCER);
-
-        SpotModel spotWithInvalidDuration = new SpotModel(null, "", COMPANY,
-                ANNOUNCER, LocalDate.now(), null, true, 20.00);
-
-        assertThatThrownBy(() -> spotRepository.save(spotWithInvalidDuration)).isInstanceOf(RuntimeException.class);
-    }
-
-    @Test
-    public void createSpot_WithInvalidPrice_ThrowsException() {
-        companyRepository.save(COMPANY);
-        announcerRepository.save(ANNOUNCER);
-
-        SpotModel spotWithInvalidPrice = new SpotModel(null, "", COMPANY,
-                ANNOUNCER, LocalDate.now(), 0.45, true, null);
-
-        assertThatThrownBy(() -> spotRepository.save(spotWithInvalidPrice)).isInstanceOf(RuntimeException.class);
-    }
-
-    @Test
     public void getAmountRaised_ReturnsTotalAmount() {
         companyRepository.save(COMPANY);
         announcerRepository.save(ANNOUNCER);
 
-        testEntityManager.persist(SPOT);
+        testEntityManager.persist(SPOT_WITH_NON_NULL_PRICE);
 
         Double amountRaised = spotRepository.getAmountRaised();
 
-        assertThat(amountRaised).isEqualTo(new BigDecimal("20.00"));
+        assertThat(amountRaised).isNotNull();
+        assertThat(amountRaised).isEqualTo(25.00);
     }
 
     @Sql(scripts = "/import_spots.sql")
